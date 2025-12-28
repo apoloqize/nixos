d2201e68c76ff9f257d38663b94c52bc5ec1bf6a
apoloqize <apoloqize@users.noreply.github.com>
2025-12-28 20:34:57 +0100

    no longer needed age files

--- secrets.nix
+++
@@ -1,8 +0,0 @@
-let
-  # Age public key for rmbp host
-  rmbp = "age1vaggp67u6566yqrpfl8kgasew7mp6gvuevhcsn4ef0pdv477tdyqaw4fsk";
-in
-{
-  "hosts/rmbp/tailscale-authkey.age".publicKeys = [ rmbp ];
-  "hosts/rmbp/tailscale-login-server.age".publicKeys = [ rmbp ];
-}
